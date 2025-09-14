99bce1919a22093fae8e161199429868a0194d32
Rentoki <knowingisnotenoughwemustapply1@gmail.com>
2025-09-14 12:31:22 +0800

    Add WindowPreferencesService

---
+++ src/main/java/com/rentoki/umafx/service/WindowPreferencesService.java
@@ -0,0 +1,35 @@
+package com.rentoki.umafx.service;
+
+import com.rentoki.umafx.interfaces.PreferencesRepository;
+
+public class WindowPreferencesService {
+    private static final String WINDOW_POS_X = "window_pos_x";
+    private static final String WINDOW_POS_Y = "window_pos_y";
+
+    private final PreferencesRepository preferencesRepository;
+
+    public WindowPreferencesService(PreferencesRepository preferencesRepository) {
+        this.preferencesRepository = preferencesRepository;
+    }
+
+    public void savePos(double posX, double posY) {
+        savePosX(posX);
+        savePosY(posY);
+    }
+
+    public void savePosX(double posX) {
+        preferencesRepository.putDouble(WINDOW_POS_X, posX);
+    }
+
+    public void savePosY(double posY) {
+        preferencesRepository.putDouble(WINDOW_POS_Y, posY);
+    }
+
+    public double getPosX() {
+        return preferencesRepository.getDouble(WINDOW_POS_X, Double.NaN);
+    }
+
+    public double getPosY() {
+        return preferencesRepository.getDouble(WINDOW_POS_Y, Double.NaN);
+    }
+}
