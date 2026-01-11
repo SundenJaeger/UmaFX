588c38c33fad6423d5e161325ceba3f9442a9272
Rentoki <knowingisnotenoughwemustapply1@gmail.com>
2026-01-12 00:27:19 +0800

    Refactor MainApplication and integrate TrayIconManager

--- src/main/java/com/rentoki/umafx/MainApplication.java
+++ src/main/java/com/rentoki/umafx/MainApplication.java
@@ -1,44 +1,37 @@
 package com.rentoki.umafx;
 
-import com.dustinredmond.fxtrayicon.FXTrayIcon;
 import com.rentoki.umafx.controller.JukeboxController;
 import com.rentoki.umafx.enums.MediaResources;
 import com.rentoki.umafx.enums.View;
-import com.rentoki.umafx.exceptions.EmptySongListException;
-import com.rentoki.umafx.exceptions.MediaPlayerException;
 import com.rentoki.umafx.exceptions.MediaResourcesException;
-import com.rentoki.umafx.exceptions.PreferencesRepositoryException;
 import com.rentoki.umafx.interfaces.PreferencesRepository;
 import com.rentoki.umafx.interfaces.PropertiesRepository;
 import com.rentoki.umafx.manager.MediaPlayerManager;
+import com.rentoki.umafx.manager.TrayIconManager;
 import com.rentoki.umafx.repository.PreferencesRepositoryImpl;
 import com.rentoki.umafx.repository.PropertiesRepositoryImpl;
 import com.rentoki.umafx.service.CharacterPropertiesService;
 import com.rentoki.umafx.service.WindowPreferencesService;
-import com.rentoki.umafx.util.ShowAlert;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckMenuItem;
-import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
+    private static final String APP_TITLE = "UmaFX";
+
+    private final MediaPlayerManager mediaPlayerManager = new MediaPlayerManager();
+    private TrayIconManager trayIconManager;
+
     private final CharacterPropertiesService characterPropertiesService;
     private final WindowPreferencesService windowPreferencesService;
-    private final MediaPlayerManager mediaPlayerManager = new MediaPlayerManager();
-    private final BooleanProperty volumeVisible = new SimpleBooleanProperty(false);
 
-    private Stage primaryStage;
     private JukeboxController jukeboxController;
 
     public MainApplication() {
@@ -51,143 +44,81 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        this.primaryStage = stage;
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(View.JUKEBOX.getFxmlPath()));
-        fxmlLoader.setControllerFactory(param -> {
-            if (param == JukeboxController.class) {
-                return new JukeboxController(windowPreferencesService, characterPropertiesService, mediaPlayerManager, this);
-            } else {
-                try {
-                    return param.getDeclaredConstructor().newInstance();
-                } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
-                         InvocationTargetException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        initializeTray(stage);
+        Scene scene = loadScene();
+        configureStage(stage, scene);
+        restoreWindowPosition(stage);
+        setupShutdown(stage);
+    }
 
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        jukeboxController = fxmlLoader.getController();
+    /* ---------------- Helpers ---------------- */
+
+    private void initializeTray(Stage stage) {
+        trayIconManager = new TrayIconManager(stage, mediaPlayerManager, () -> jukeboxController.openSongQueue(), windowPreferencesService::savePos);
+        trayIconManager.show();
+    }
+
+    private Scene loadScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource(View.JUKEBOX.getFxmlPath()));
+
+        loader.setControllerFactory(this::createController);
+
+        Scene scene = new Scene(loader.load(), 320, 240);
+        jukeboxController = loader.getController();
 
         characterPropertiesService.initializeProperties();
 
-        scene.setFill(Color.TRANSPARENT);
-        scene.getStylesheets().add(MainApplication.class.getResource("css/base.css").toExternalForm());
+        configureScene(scene);
 
-        stage.setTitle("UmaFX");
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setScene(scene);
-        stage.show();
+        return scene;
+    }
+
+    private Object createController(Class<?> clazz) {
+        if (clazz == JukeboxController.class) {
+            return new JukeboxController(windowPreferencesService, characterPropertiesService, mediaPlayerManager, trayIconManager);
+        }
 
         try {
-            stage.getIcons().add(MediaResources.APP_ICON.getImage(MainApplication.class));
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Failed to create controller: " + clazz, e);
+        }
+    }
+
+    private void configureScene(Scene scene) {
+        scene.setFill(Color.TRANSPARENT);
+        scene.getStylesheets().add(Objects.requireNonNull(MainApplication.class.getResource("css/base.css")).toExternalForm());
+    }
+
+    private void configureStage(Stage stage, Scene scene) {
+        stage.setTitle(APP_TITLE);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.setAlwaysOnTop(true);
+
+        loadStageIcon(stage);
+
+        stage.show();
+    }
+
+    private void loadStageIcon(Stage stage) {
+        try {
+            stage.getIcons().add(MediaResources.APP_ICON.getImage());
         } catch (MediaResourcesException e) {
             stage.getIcons().clear();
         }
+    }
 
+    private void restoreWindowPosition(Stage stage) {
         stage.setX(windowPreferencesService.getPosX());
         stage.setY(windowPreferencesService.getPosY());
-        stage.setAlwaysOnTop(true);
-        stage.setOnCloseRequest(event -> {
+    }
+
+    private void setupShutdown(Stage stage) {
+        stage.setOnCloseRequest(windowEvent -> {
             Platform.setImplicitExit(true);
             Platform.exit();
             System.exit(0);
         });
-
-        setTrayIcon();
-    }
-
-    public MenuItem playPauseItem() {
-        MenuItem menuItem = new MenuItem();
-
-        menuItem.textProperty().bind(Bindings.when(mediaPlayerManager.playingProperty()).then("Pause").otherwise("Play"));
-        menuItem.disableProperty().bind(Bindings.isEmpty(mediaPlayerManager.getSongs()));
-
-        menuItem.setOnAction(event -> {
-            if (mediaPlayerManager.isPlaying()) {
-                safeRun(mediaPlayerManager::pause);
-            } else {
-                safeRun(mediaPlayerManager::play);
-            }
-        });
-
-        return menuItem;
-    }
-
-    public MenuItem stopItem() {
-        MenuItem menuItem = new MenuItem("Stop");
-
-        menuItem.setOnAction(event -> safeRun(mediaPlayerManager::stop));
-        menuItem.disableProperty().bind(Bindings.isEmpty(mediaPlayerManager.getSongs()));
-
-        return menuItem;
-    }
-
-    public MenuItem skipItem() {
-        MenuItem menuItem = new MenuItem("Skip");
-
-        menuItem.setOnAction(event -> safeRun(mediaPlayerManager::skip));
-        menuItem.disableProperty().bind(Bindings.isEmpty(mediaPlayerManager.getSongs()));
-
-        return menuItem;
-    }
-
-    public CheckMenuItem volumeItem() {
-        CheckMenuItem checkMenuItem = new CheckMenuItem("Volume");
-
-        checkMenuItem.disableProperty().bind(Bindings.isEmpty(mediaPlayerManager.getSongs()));
-        checkMenuItem.selectedProperty().bindBidirectional(volumeVisible);
-
-        return checkMenuItem;
-    }
-
-    public BooleanProperty volumeVisibleProperty() {
-        return volumeVisible;
-    }
-
-    private void setTrayIcon() {
-        FXTrayIcon fxTrayIcon;
-
-        try {
-            fxTrayIcon = new FXTrayIcon(primaryStage, MediaResources.APP_ICON.getImage(MainApplication.class));
-        } catch (MediaResourcesException e) {
-            fxTrayIcon = new FXTrayIcon(primaryStage);
-        }
-
-        fxTrayIcon.addMenuItem("Reset Window", event -> {
-            primaryStage.setX(0);
-            primaryStage.setY(0);
-
-            try {
-                windowPreferencesService.savePos(0, 0);
-            } catch (PreferencesRepositoryException e) {
-                ShowAlert.showError(e.getMessage());
-            }
-        });
-        fxTrayIcon.insertSeparator(1);
-        fxTrayIcon.addMenuItem("Open Song Queue", event -> jukeboxController.openSongQueue());
-        fxTrayIcon.addSeparator();
-        fxTrayIcon.addMenuItem(playPauseItem());
-        fxTrayIcon.addMenuItem(stopItem());
-        fxTrayIcon.addMenuItem(skipItem());
-        fxTrayIcon.addMenuItem(volumeItem());
-        fxTrayIcon.addSeparator();
-        fxTrayIcon.addExitItem("Exit UmaFX");
-        fxTrayIcon.show();
-    }
-
-    private void safeRun(RunnableWithException action) {
-        try {
-            action.run();
-        } catch (EmptySongListException e) {
-            ShowAlert.error().text("Error", e.getMessage()).showAndWait().ifPresent(buttonType -> jukeboxController.openSongQueue());
-        } catch (MediaPlayerException e) {
-            ShowAlert.showError(e.getMessage());
-        }
-    }
-
-    @FunctionalInterface
-    private interface RunnableWithException {
-        void run() throws MediaPlayerException, EmptySongListException;
     }
 }
